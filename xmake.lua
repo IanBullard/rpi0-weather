1c0b62d12ef51cae777cabe6815b99f4d40ba4de
Ian Bullard <ian@bullard.cc>
2023-01-19 17:53:10 -0600

    Fixing xmake file

--- xmake.lua
+++ xmake.lua
@@ -1,8 +1,4 @@
 set_languages("cxx17")
-add_rules("mode.debug", "mode.release")
-if is_plat("windows") then
-    set_config("cxxflags", "/Zc:__cplusplus")
-end
 
 requires = {"freeimage", "freetype", "glm", "libsdl", "libzip", "fmt", "python", "sqlite3", "rapidjson", "utfcpp"}
 add_requires(requires)
@@ -12,4 +8,7 @@ target("weather")
     add_files("src/*.cpp")
     add_files("src/utils/*.cpp")
     add_packages(requires)
-    add_defines("-std:c++17")
+
+    if is_plat("windows") then
+        add_cxxflags("/Zc:__cplusplus")
+    end
