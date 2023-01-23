d8b0ca0c3c1026b082dc97c13de90d96c813069a
Ian Bullard <ian@bullard.cc>
2023-01-23 17:08:48 -0600

    Cleanup up setup script

--- setup.sh
+++ setup.sh
@@ -1,8 +1,4 @@
 #!/bin/sh
 # This script is used to setup the Pi 0 with the proper software needed to run this project.
 #   The end result should be a rpi0-weather that is ready to run
-sudo apt install git
-git clone https://github.com/IanBullard/rpi0-weather.git
-python -m ensurepip --upgrade
-pip install requests
-pip install python-dateutil
+pip install requests python-dateutil
